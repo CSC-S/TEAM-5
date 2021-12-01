9ff9b957e8fad7f54dadb988d106015b9dbb2f90
dearmmww <dearmmww@gmail.com>
2021-12-01 20:00:33 +0900

    first commit

---
+++ src/pages/ContestBoard.js
@@ -0,0 +1,18 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+import ".././App.css";
+
+const ContestBoard = () => {
+    return (
+        <div>
+            <Header></Header>
+            
+            
+            
+            <Footer></Footer>
+        </div>
+);
+};
+
+export default ContestBoard;
